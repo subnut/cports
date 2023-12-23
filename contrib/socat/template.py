c51d390e3d38c5c4ca0d96eabfa9c6a20de3d5ef
Subhaditya Nath <sn03.general@gmail.com>
2023-12-23 10:08:20 +0530

    contrib/socat: remove dependency on bash, fix test

--- contrib/socat/template.py
+++ contrib/socat/template.py
@@ -1,22 +1,31 @@
 pkgname = "socat"
 pkgver = "1.8.0.0"
-pkgrel = 0
-build_style = "gnu_configure"
-# broken
-configure_gen = []
-make_dir = "."
+pkgrel = 1
+build_style = "configure"
+make_install_args = ["prefix=/usr"]
 make_check_target = "test"
-makedepends = [
-    "linux-headers",
-    "openssl-devel",
-    "readline-devel",
-]
-pkgdesc = "Multipurpose relay for binary protocols"
-maintainer = "psykose <alice@ayaya.dev>"
+makedepends = ["linux-headers", "openssl-devel", "readline-devel"]
+checkdepends = ["bash", "iproute2", "procps"]
+pkgdesc = "Multipurpose relay for bidirectional binary data transfer (netcat++)"
+maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
 license = "GPL-2.0-only"
 url = "http://www.dest-unreach.org/socat"
-source = f"{url}/download/socat-{pkgver}.tar.gz"
-sha256 = "6010f4f311e5ebe0e63c77f78613d264253680006ac8979f52b0711a9a231e82"
+source = f"{url}/download/socat-{pkgver}.tar.bz2"
+sha256 = "e1de683dd22ee0e3a6c6bbff269abe18ab0c9d7eb650204f125155b9005faca7"
 hardening = ["vis", "cfi"]
-# needs a smorgasbord of random network utilities
-options = ["!check"]
+
+
+def post_extract(self):
+    # tests 478 and 528 fail due to stdout not being a tty
+    self.do(
+        "sed",
+        "-i",
+        ".bak",
+        "/^test:/,+1s|./test.sh|& --expect-fail 478,528|",
+        "Makefile.in",
+    )
+    # test.sh uses $USER, which is unset in the environment
+    # if $USER is unset, test 418 fails (due to user=$USER argument of socat)
+    #
+    # but if $USER is set, test 217 fails, and I've got NO IDEA WHY!
+    self.do("sed", "-i", ".in", "s/$USER/$(whoami)/g", "test.sh")
