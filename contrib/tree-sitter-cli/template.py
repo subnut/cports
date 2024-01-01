9643dcb0342957f798300da85a99a255d707c400
Subhaditya Nath <sn03.general@gmail.com>
2024-01-01 12:07:08 +0530

    contrib/tree-sitter-cli: new package (0.20.8)

---
+++ contrib/tree-sitter-cli/template.py
@@ -0,0 +1,24 @@
+pkgname = "tree-sitter-cli"
+pkgver = "0.20.8"
+pkgrel = 0
+build_wrksrc = "cli"
+build_style = "cargo"
+hostmakedepends = ["cargo"]
+pkgdesc = "Parser generator tool and incremental parsing library (CLI tool)"
+maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
+license = "MIT"
+url = "https://tree-sitter.github.io"
+source = f"https://github.com/tree-sitter/tree-sitter/archive/v{pkgver}.tar.gz"
+sha256 = "6181ede0b7470bfca37e293e7d5dc1d16469b9485d13f13a605baec4a8b1f791"
+# tests require remote repos to be vendored
+options = ["!check"]
+
+
+def post_extract(self):
+    from cbuild.util import patch
+
+    patch.patch(self, self.files_path / "fix-getrandom.patch")
+
+
+def post_install(self):
+    self.install_license("../LICENSE")
