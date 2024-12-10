8f5b2359df9fcc2e262968072efa6b8015416034
Subhaditya Nath <sn03.general@gmail.com>
2024-12-10 23:04:58 +0530

    user/massren: new package

---
+++ user/massren/template.py
@@ -0,0 +1,24 @@
+pkgname = "massren"
+pkgver = "1.5.7"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+makedepends = ["sqlite-devel"]
+go_build_tags = ["libsqlite3"]
+go_check_tags = ["libsqlite3"]
+pkgdesc = "Rename multiple files using any text editor"
+maintainer = "Subhaditya Nath <sn03.general@gmail.com>"
+license = "MIT"
+url = "https://github.com/laurent22/massren"
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "7e7dd149bd3364235247268cc684b5a35badd9bee22f39960e075c792d037a8c"
+hardening = ["vis"]
+
+
+def post_extract(self):
+    self.rm("vendor", True)
+
+
+def post_install(self):
+    # License extracted from the end of README.md
+    self.install_license(self.files_path / "LICENSE")
