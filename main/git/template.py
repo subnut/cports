8c75fc1ee2e8524d18c937da6670deb15b666023
Subhaditya Nath <sn03.general@gmail.com>
2025-04-03 03:03:00 +0530

    main/git: WIP meson build

--- main/git/template.py
+++ main/git/template.py
@@ -1,9 +1,18 @@
 pkgname = "git"
 pkgver = "2.49.0"
 pkgrel = 0
+build_style = "meson"
+configure_args = [
+    "--libexecdir=/usr/lib",  # XXX drop libexec
+    "-Dcontrib=completion,contacts,subtree",
+    "-Dcredential_helpers=libsecret",
+    "-Ddocs=man",
+]
 hostmakedepends = [
     "asciidoc",
+    "cmake",
     "gettext",
+    "meson",
     "perl",
     "pkgconf",
     "tk",
@@ -15,6 +24,7 @@ makedepends = [
     "libsecret-devel",
     "pcre2-devel",
     "tk-devel",
+    "zlib-ng-devel",
 ]
 depends = [
     "ca-certificates",
@@ -31,96 +41,6 @@ sha256 = "618190cf590b7e9f6c11f91f23b1d267cd98c3ab33b850416d8758f8b5a85628"
 hardening = ["cfi", "vis"]
 
 
-def configure(self):
-    with open(self.cwd / "config.mak", "w") as cf:
-        cf.write(
-            f"""
-prefix = /usr
-gitexecdir = /usr/lib/git-core
-CC = {self.get_tool("CC")}
-AR = {self.get_tool("AR")}
-TAR = tar
-CFLAGS = {self.get_cflags(shell=True)}
-LDFLAGS = {self.get_ldflags(shell=True)}
-USE_LIBPCRE2 = Yes
-NO_INSTALL_HARDLINKS = Yes
-ICONV_OMITS_BOM = Yes
-NO_REGEX = Yes
-INSTALLDIRS = vendor
-INSTALL_SYMLINKS = 1
-perllibdir = /usr/share/perl5/vendor_perl
-PYTHON_PATH = /usr/bin/python
-DEFAULT_TEST_TARGET=prove
-GIT_PROVE_OPTS=--jobs={self.make_jobs}
-HOST_CPU = {self.profile().arch}
-"""
-        )
-
-
-def build(self):
-    cmd = ["make", f"-j{self.make_jobs}"]
-    self.do(*cmd)
-    self.do(*cmd, "-C", "Documentation", "man")
-    self.do(*cmd, "-C", "contrib/contacts", "all", "git-contacts.1")
-    self.do(*cmd, "-C", "contrib/diff-highlight", "all")
-    self.do(*cmd, "-C", "contrib/subtree", "all", "man")
-    self.do(*cmd, "-C", "contrib/credential/libsecret", "all")
-
-
-def check(self):
-    self.do("make", "test")
-    self.do("make", "-C", "contrib/diff-highlight", "test")
-    self.do("make", "-C", "contrib/subtree", "test")
-
-
-def install(self):
-    ddir = f"DESTDIR={self.chroot_destdir}"
-    self.do("make", "install", "install-doc", ddir)
-    self.do("make", "-C", "contrib/contacts", "install", "install-man", ddir)
-    self.do("make", "-C", "contrib/subtree", "install", "install-man", ddir)
-    # no install target
-    self.install_file(
-        "contrib/credential/libsecret/git-credential-libsecret",
-        "usr/lib/git-core",
-        mode=0o755,
-    )
-
-    # remove cvs for now
-    self.uninstall("usr/bin/git-cvsserver")
-    self.uninstall("usr/lib/git-core/git-cvsexportcommit")
-    self.uninstall("usr/lib/git-core/git-cvsimport")
-    self.uninstall("usr/lib/git-core/git-cvsserver")
-    self.uninstall("usr/share/man/man1/git-cvsexportcommit.1")
-    self.uninstall("usr/share/man/man1/git-cvsimport.1")
-    self.uninstall("usr/share/man/man1/git-cvsserver.1")
-    self.uninstall("usr/share/man/man7/gitcvs-migration.7")
-
-    self.install_completion("contrib/completion/git-completion.bash", "bash")
-    self.install_file("contrib/completion/git-prompt.sh", "usr/share/git")
-
-    self.install_bin("contrib/diff-highlight/diff-highlight")
-    self.install_file(
-        "contrib/diff-highlight/README",
-        "usr/share/doc/git",
-        name="diff-highlight",
-    )
-
-    self.install_file(
-        "contrib/git-jump/git-jump", "usr/lib/git-core", mode=0o755
-    )
-    self.install_file(
-        "contrib/git-jump/README", "usr/share/doc/git", name="git-jump"
-    )
-
-    # hardlink
-    p = self.destdir / "usr/lib/git-core/git-citool"
-    self.rm(p)
-    p.symlink_to("git-gui")
-
-    # register shells
-    self.install_shell("/usr/bin/git-shell")
-
-
 @subpackage("git-gitk")
 def _(self):
     self.depends += [self.parent, "tk"]
@@ -130,18 +50,18 @@ def _(self):
     return ["usr/bin/gitk", "usr/share/gitk", "usr/share/man/man1/gitk.1"]
 
 
-@subpackage("git-gui")
-def _(self):
-    self.depends += [self.parent, "tk"]
-    self.subdesc = "GUI tool"
-    self.license = "GPL-2.0-or-later"
-    return [
-        "usr/lib/git-core/git-gui*",
-        "usr/lib/git-core/git-citool",
-        "usr/share/man/man1/git-gui.1",
-        "usr/share/man/man1/git-citool.1",
-        "usr/share/git-gui",
-    ]
+# @subpackage("git-gui")
+# def _(self):
+#     self.depends += [self.parent, "tk"]
+#     self.subdesc = "GUI tool"
+#     self.license = "GPL-2.0-or-later"
+#     return [
+#         "usr/lib/git-core/git-gui*",
+#         "usr/lib/git-core/git-citool",
+#         "usr/share/man/man1/git-gui.1",
+#         "usr/share/man/man1/git-citool.1",
+#         "usr/share/git-gui",
+#     ]
 
 
 @subpackage("git-credential-libsecret")
