1b2f08f78efc2900a08476832f6a081394ee4cb4
Subhaditya Nath <sn03.general@gmail.com>
2025-06-18 23:41:54 +0530

    user/lf: update to 35

--- user/lf/template.py
+++ user/lf/template.py
@@ -1,13 +1,13 @@
 pkgname = "lf"
-pkgver = "34"
-pkgrel = 5
+pkgver = "35"
+pkgrel = 0
 build_style = "go"
 hostmakedepends = ["go"]
 pkgdesc = "Terminal file manager"
 license = "MIT"
 url = "https://github.com/gokcehan/lf"
 source = f"{url}/archive/refs/tags/r{pkgver}.tar.gz"
-sha256 = "9c78735fa88c0b77664d7de41e7edbbca99ace5410f522530307244a09839263"
+sha256 = "bf2ecd63eb75ceeb88f42977d49515bbd755b7f3d4db651a097c6ba47fd2d989"
 
 
 def post_install(self):
